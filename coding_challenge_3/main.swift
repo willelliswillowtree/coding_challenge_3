be8e81aec8358f4ff8cd8bf27ac97c1b8478805c
willelliswillowtree <will.ellis@willowtreeapps.com>
2016-05-13 15:08:10 -0400

    Add stdin line parser

--- coding_challenge_3/main.swift
+++ coding_challenge_3/main.swift
@@ -8,5 +8,19 @@
 
 import Foundation
 
-print("Hello, World!")
+let ramCount = 10
 
+var ram = Array(count: ramCount, repeatedValue: 0)
+
+var idx = 0
+while idx < ramCount, let line = readLine(stripNewline: true) where line.characters.count != 0 {
+    guard line.characters.count == 3, let word = Int(line) else {
+        exit (1)
+    }
+    ram[idx] = word
+    idx += 1
+}
+
+
+
+print("Ram: \(ram.map({ "\($0)" }).joinWithSeparator(" "))")
