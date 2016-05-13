8d7fce1050cb9f0e3a53c783169bfd0131e2d7c5
willelliswillowtree <will.ellis@willowtreeapps.com>
2016-05-13 13:43:20 -0400

    Initial Commit

---
+++ coding_challenge_3/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  coding_challenge_3
+//
+//  Created by Will Ellis on 5/13/16.
+//  Copyright © 2016 Will Ellis. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
